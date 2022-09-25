5b28cdc5ea841cce220a6b9f7657014e20447a3e
seovulf <seovulf@gmail.com>
2022-09-25 03:37:18 +0300

    Initial commit

---
+++ android/src/com/seeoff/gucciflipflopsbird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.seeoff.gucciflipflopsbird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.seeoff.gucciflipflopsbird.FlipFlop;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlipFlop(), config);
+	}
+}
